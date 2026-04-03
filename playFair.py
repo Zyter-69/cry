2030f59c3cc8e1b38c28ea59c6addf2c2fec1ba9
Zyter_69 <ayoub.meftah2006@gmail.com>
2026-04-03 10:38:30 +0100

    playfair done

--- playFair.py
+++ playFair.py
@@ -15,7 +15,9 @@ def printMatrix(table):
         print()
 
 
-def enterKey(key):
+def enterKey():
+    key = input("Enter the key: ")
+    
     alphabet = "abcdefghijklmnopqrstuvxyz"# w mkach
     key = key.replace(" ", "")
     key = key.lower()
@@ -34,7 +36,20 @@ def enterKey(key):
     
     return result
 
-def encrypt(text, table):
+def inTheTable (char , table):
+    x = False 
+    for i in range (5):
+        for j in range (5):
+            if char == table[i][j] :
+                x = True
+    return x
+
+
+
+def encrypt(table):
+    
+    print("enter the text to encrypt : ")
+    text = input()
     text = text.replace(" ", "")
     text = text.lower()
     i = 0
@@ -50,8 +65,15 @@ def encrypt(text, table):
     for i in range(0, len(text), 2):
         doubleChar = text[i:i + 2]
         char1 = doubleChar[0]
-        char2 = doubleChar[1] if len(doubleChar) > 1 else 'x'  # Pad with 'x' if there's an odd character   
-        if char1 in table and char2 in table :
+        char2 = doubleChar[1] if len(doubleChar) > 1 else 'x'  # Pad with 'x' if there's an odd character 
+        if char1 =='w' or char2 == 'w':
+            print("The letter 'w' is not allowed in the Playfair cipher. it will be replaced by x.")
+            if char1 == 'w':
+                char1 = 'x'
+            if char2 == 'w':
+                char2 = 'x'
+
+        if inTheTable(char1, table) and inTheTable(char2, table):
             row1, col1 = [(row, col) for row in range(5) for col in range(5) if table[row][col] == char1][0]
             row2, col2 = [(row, col) for row in range(5) for col in range(5) if table[row][col] == char2][0]
             
@@ -64,21 +86,55 @@ def encrypt(text, table):
             else:
                 encryptedText += table[row1][col2]
                 encryptedText += table[row2][col1]
+    return encryptedText
 
+def decrypt (table):
+    text = input ("enter encrypted text to decrypted : ")
+    text = text.replace(" ", "")
+    text = text.lower()
 
+    decryptedText = ""
+    for i in range(0 , len(text) , 2):
+        doubleChar = text [i : i+2]
+        char1 = doubleChar[0]
+        char2 = doubleChar[1]
 
+        if inTheTable(char1, table) and inTheTable(char2, table):
+            row1 , col1 = [(row , col) for row in range (5)for col in range (5) if table[row][col] == char1][0]
+            row2 , col2 = [(row , col) for row in range (5)for col in range (5) if table[row][col] == char2][0]
+            if row1 == row2:
+                decryptedText += table[row1][(col1 - 1) % 5]
+                decryptedText += table[row2][(col2 - 1) % 5]
+            elif col1 == col2:
+                decryptedText += table[(row1 - 1) % 5][col1]
+                decryptedText += table[(row2 - 1) % 5][col2]
+            else:
+                decryptedText += table[row1][col2]
+                decryptedText += table[row2][col1]
+    y = int (input("do you want to remove the 'x' that was added for padding ? 1 for yes and 2 for no : "))
+    if y == 1:
+        decryptedText = decryptedText.replace('x', '')
+    return decryptedText
 
 
 def main():
     print("Welcome to the Playfair cipher program!")
-    key = input("Enter the key: ")
-    keyList = enterKey(key)
-    table = createMatrix(keyList)
-    printMatrix(table)
-    print("enter the text to encrypt : ")
-    text = input()
-    encrypt(text, table)
+    while True:
+        print("Enter 1 for encryption, 2 for decryption, or 3 to exit: ")
+        choice = input()
+        if choice == "3":
+            break
+        elif choice == "1":
 
+            keyList = enterKey()
+            table = createMatrix(keyList)
+            printMatrix(table)
+            print("the encrypted text : " + encrypt(table))
+        elif choice == "2":
+            keyList = enterKey()
+            table = createMatrix(keyList)
+            printMatrix(table)
+            print("the decrypted text : " + decrypt(table))
 
 if __name__ == "__main__":
     main()
