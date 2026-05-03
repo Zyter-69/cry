51bc2d57b2010dd10749c7b1e8c67e7da5ce9aa2
abdelwahabkr <abdelwahabkdr12@gmail.com>
2026-05-03 18:32:58 +0100

    md5

---
+++ MD5.py
@@ -0,0 +1,100 @@
+import math
+
+import math
+import struct
+
+def pad(msg):
+    msg_bytes = msg.encode('utf-8')
+    length = len(msg_bytes) * 8
+    msg_bytes += b'\x80'
+    while len(msg_bytes) % 64 != 56:
+        msg_bytes += b'\x00'
+    msg_bytes += struct.pack('<Q', length) 
+    return msg_bytes
+
+def split(msg_bytes):
+    return [msg_bytes[i:i+64] for i in range(0, len(msg_bytes), 64)]
+
+def FF(a, b, c, d, M, s, i):
+    F = (b & c) | ((~b & 0xFFFFFFFF) & d)          
+    ti = int((2**32) * abs(math.sin(i + 1))) & 0xFFFFFFFF
+    x = (a + F + M + ti) & 0xFFFFFFFF               
+    rotated = ((x << s) | (x >> (32 - s))) & 0xFFFFFFFF
+    new_a = (b + rotated) & 0xFFFFFFFF              
+    return new_a, b, c, d
+
+def GG(a, b, c, d, M, s, i):
+    G = (b & d) | (c & (~d & 0xFFFFFFFF))   
+    ti = int((2**32) * abs(math.sin(i + 1))) & 0xFFFFFFFF
+    x = (a + G + M + ti) & 0xFFFFFFFF
+    rotated = ((x << s) | (x >> (32 - s))) & 0xFFFFFFFF
+    new_a = (b + rotated) & 0xFFFFFFFF
+    return new_a, b, c, d
+
+def HH(a, b, c, d, M, s, i):
+    H = b ^ c ^ d                           
+    ti = int((2**32) * abs(math.sin(i + 1))) & 0xFFFFFFFF
+    x = (a + H + M + ti) & 0xFFFFFFFF
+    rotated = ((x << s) | (x >> (32 - s))) & 0xFFFFFFFF
+    new_a = (b + rotated) & 0xFFFFFFFF
+    return new_a, b, c, d
+
+def II(a, b, c, d, M, s, i):
+    I = c ^ (b | (~d & 0xFFFFFFFF))          
+    ti = int((2**32) * abs(math.sin(i + 1))) & 0xFFFFFFFF
+    x = (a + I + M + ti) & 0xFFFFFFFF
+    rotated = ((x << s) | (x >> (32 - s))) & 0xFFFFFFFF
+    new_a = (b + rotated) & 0xFFFFFFFF
+    return new_a, b, c, d
+
+
+def MD5(msg):
+    msg_bytes = pad(msg)
+    blocks = split(msg_bytes)
+    
+    for block in blocks:
+        a = 0x67452301
+        b = 0xEFCDAB89
+        c = 0x98BADCFE
+        d = 0x10325476
+        
+        M = list(struct.unpack('<16I', block))
+        
+        for j in range(0, 16, 4):
+            a, b, c, d = FF(a, b, c, d, M[j],   7,  j)
+            d, a, b, c = FF(d, a, b, c, M[j+1], 12, j+1)
+            c, d, a, b = FF(c, d, a, b, M[j+2], 17, j+2)
+            b, c, d, a = FF(b, c, d, a, M[j+3], 22, j+3)
+
+        i=1
+        for j in range(0, 16, 4):
+            a, b, c, d = GG(a, b, c, d, M[i],   5,  j+16)
+            i=(i+5)%16
+            d, a, b, c = GG(d, a, b, c, M[i],  9, j+1+16)
+            i=(i+5)%16
+            c, d, a, b = GG(c, d, a, b, M[i], 14, j+2+16)
+            i=(i+5)%16
+            b, c, d, a = GG(b, c, d, a, M[i], 20, j+3+16)
+            i=(i+5)%16
+        i=5
+        for j in range(0, 16, 4):
+            a, b, c, d = HH(a, b, c, d, M[i],   4,  j+32)
+            i=(i+3)%16
+            d, a, b, c = HH(d, a, b, c, M[i], 11, j+1+32)
+            i=(i+3)%16
+            c, d, a, b = HH(c, d, a, b, M[i], 16, j+2+32)
+            i=(i+3)%16
+            b, c, d, a = HH(b, c, d, a, M[i], 23, j+3+32)
+            i=(i+3)%16
+        i=0
+        for j in range(0, 16, 4):
+            a, b, c, d = II(a, b, c, d, M[i],   6,  j+48)
+            i=(i+7)%16
+            d, a, b, c = II(d, a, b, c, M[i], 10, j+1+48)
+            i=(i+7)%16
+            c, d, a, b = II(c, d, a, b, M[i], 15, j+2+48)
+            i=(i+7)%16
+            b, c, d, a = II(b, c, d, a, M[i], 21, j+3+48)
+            i=(i+7)%16
+        
+        print(f"A = {hex(a)}\nB = {hex(b)}\nC = {hex(c)}\nD = {hex(d)}")
